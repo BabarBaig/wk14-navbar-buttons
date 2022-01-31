17984713315cd22c03a980ec8439638a0041e19d
Babar Baig <BobBaig@outlook.com>
2022-01-31 00:37:46 -0500

    Use anonymous function to pass multiple args to moveToCard()

--- navBarButton3.jsx
+++ navBarButton3.jsx
@@ -1,3 +1,7 @@
+/* In this file, we’ll learn how to pass onClick events into an anonymous function,
+ * which will allow us to pass multiple arguments. We'll pass events to moveToCart(),
+ * which will enable us to pass products from the list to the cart when they are clicked.
+*/
 // Ex 3 - write out all items with their stock number.
 // provide a button and use onClick={moveToCart} to move 1 item into the Shopping Cart
 // use React.useState to keep track of items in the Cart.
@@ -8,11 +12,12 @@ function NavBar({ stockitems }) {
   const [stock, setStock] = React.useState(stockitems);
   const { Button } = ReactBootstrap;
 
-  // event apple:2
-  const moveToCart = e => {
-    let [name, num] = e.target.innerHTML.split(":"); // innerHTML should be format name:3
-    // use newStock = stock.map to find "name" and decrease number in stock by 1
-    // only if instock is >=  do we move item to Cart and update stock
+  // Note: even can get only 1 argument. But say we want item name:id?
+  // IDEA: Use anonymous function to concat the 2 values into 1 string.
+  // E.g. event apple:2
+  const moveToCart = (id, e) => {
+    console.log(id)
+    let [name, num] = e.target.innerHTML.split(":");
     let newStock = stock.map((item, index) => {
       if (item.name == name) item.instock--;
       return item;
@@ -21,10 +26,11 @@ function NavBar({ stockitems }) {
   };
 
   const updatedList = stock.map((item, index) => {
+    // We want to send 2+ parameters to moveToCard(), but onClick will
+    // only allow one argument to be passed.  IDEA: Use anonymous function
+    // to concat the 2 values into 1 string. E.g. event apple:2
     return (
-      <Button onClick={moveToCart} key={index}>
-        {item.name}:{item.instock}
-      </Button>
+      <Button onClick={e => moveToCart({id:1}, e)} key={index}>{item.name}:{item.instock}</Button>
     );
   });
 

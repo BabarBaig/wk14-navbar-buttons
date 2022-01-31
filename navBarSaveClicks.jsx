98d8f40ba79e2cc4b1a360adc69ba8766a0784c7
Babar Baig <BobBaig@outlook.com>
2022-01-31 02:58:35 -0500

    Update navBarSaveClicks.jsx

--- navBarSaveClicks.jsx
+++ navBarSaveClicks.jsx
@@ -1,6 +1,6 @@
 // use of "props" to set data
 function NavBar(props) {
-    const [clicks, setClicks] = React.useState([]);
+    const [clicks, setClicks] = React.useState([]); // useState()=>browser re-render
     console.log(`clicks: ${clicks}`);   // clicks[] give history of button press
   
     const list = props.menuitems;
@@ -13,17 +13,16 @@ function NavBar(props) {
     };
     
     const updatedList = list.map((listItems, index) => {
-      return (
-        <Button onClick={handleClick} key={index}>{listItems}</Button>
-      );
+        return (<Button onClick={handleClick} key={index}>{listItems}</Button>);
     });
     
     // note that React needs to have a single Parent
     return <ul>{updatedList}</ul>;
-    }
-    const menuItems = [1, 2, 3, 4, 5];
-    ReactDOM.render(
-        <NavBar menuitems={menuItems} />,
-        document.getElementById("root")
+}
+
+const menuItems = [1, 2, 3, 4, 5];
+ReactDOM.render(
+    <NavBar menuitems={menuItems} />,
+    document.getElementById("root")
 );
   

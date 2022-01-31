781b83d168e8b52de71f346b7af182793bcc55c4
Babar Baig <BobBaig@outlook.com>
2022-01-31 02:28:21 -0500

    Flesh-out started code

--- navBarSaveClicks.jsx
+++ navBarSaveClicks.jsx
@@ -1,41 +1,35 @@
 // use of "props" to set data
 function NavBar(props) {
-  const [clicks, setClicks] = React.useState([]);
-  console.log(`clicks: ${clicks}`);
-  // alert(`Rendering NavBar`);
-  const list = props.menuitems;
-  const { Button } = ReactBootstrap;
-  // each item should have an unique key
-  const handlßeClick = e => {
-    console.log(`clicked on: ${e.target.innerHTML}`);
-    setClicks([...clicks, e.target.innerHTML]);
-  };
-
-  function handleClick(){
-    console.log('handleClick!')
-  }
-
-  // const updatedList = list.map((listItems, index) => {
-  //   return (
-  //     <Button onClick={handleClick} key={index}>
-  //       {listItems}
-  //     </Button>
-  //   );
-  // });
-
-  const updatedList = list.filter((listItems, index) => {
-    return (
-      <Button onClick={handleClick} key={index}>
-        {listItems}
-      </Button>
-    );
-  });
-
-  // note that React needs to have a single Parent
-  return <ul>{updatedList}</ul>;
-}
-const menuItems = [1, 2, 3, 4, 5];
-ReactDOM.render(
-  <NavBar menuitems={menuItems} />,
-  document.getElementById("root")
+    const [clicks, setClicks] = React.useState([]);
+    console.log(`clicks: ${clicks}`);   // clicks[] give history of button press
+  
+    const list = props.menuitems;
+    const { Button } = ReactBootstrap;
+  
+    // each item should have an unique key
+    const handleClick = e => {
+        // console.log(`clicked on: ${e.target.innerHTML}`);
+        setClicks([...clicks, e.target.innerHTML]);     // Record clicks
+    };
+    
+    const updatedList = list.map((listItems, index) => {
+      return (
+        <Button onClick={handleClick} key={index}>{listItems}</Button>
+      );
+    });
+  
+    // const updatedList = list.filter((listItems, index) => {
+    //     return (
+    //         <Button onClick={handleClick} key={index}>{listItems}</Button>
+    //     );
+    // });
+  
+    // note that React needs to have a single Parent
+    return <ul>{updatedList}</ul>;
+    }
+    const menuItems = [1, 2, 3, 4, 5];
+    ReactDOM.render(
+        <NavBar menuitems={menuItems} />,
+        document.getElementById("root")
 );
+  
